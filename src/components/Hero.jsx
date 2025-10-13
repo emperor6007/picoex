d3afc039142cfac151f5fd83808639ff128eada0
emperor6007 <68384816+emperor6007@users.noreply.github.com>
2025-10-13 06:22:12 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
       LTC: 150,
       BNB: 980,
       USDT: 1,
-      XRP: 2.0311,
+      XRP: 2.006778,
       DOGE: 0.21,
       SOL: 235,
       MATIC: 0.25,
@@ -474,6 +474,7 @@ const Hero = () => {
 };
 
 export default Hero;
+
 
 
 
